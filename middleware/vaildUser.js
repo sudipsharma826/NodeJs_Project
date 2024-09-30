c3b09542d98f6e605369e74a78c1554a9ccb141a
Sudip Sharma <sudpokhara826@gmail.com>
2024-09-30 17:19:22 +0545

    Message dispaly in the home page

--- middleware/vaildUser.js
+++ middleware/vaildUser.js
@@ -9,7 +9,7 @@ exports.validUser = async (req, res, next) => {
         const data = await blogs.findByPk(blogId);
         if (!data) {
             req.flash('error', 'Blog not found.');
-            return res.redirect('/blogs'); // Redirect if blog is not found
+            return res.redirect('/'); // Redirect if blog is not found
         }
         
         const blogUserId = data.registerId;
@@ -17,7 +17,7 @@ exports.validUser = async (req, res, next) => {
         // Check if the current user owns the blog
         if (blogUserId !== userId) {
             req.flash('error', 'Access Denied. You do not have permission to edit this blog.');
-            return res.redirect('/blogs'); // Redirect if access is denied
+            return res.redirect('/'); // Redirect if access is denied
         }
 
         // If user is valid, proceed to next middleware
@@ -25,6 +25,6 @@ exports.validUser = async (req, res, next) => {
     } catch (error) {
         // Handle any other unexpected errors
         req.flash('error', 'An error occurred while validating user.');
-        return res.redirect('/blogs'); // Redirect in case of an error
+        return res.redirect('/'); // Redirect in case of an error
     }
 };
